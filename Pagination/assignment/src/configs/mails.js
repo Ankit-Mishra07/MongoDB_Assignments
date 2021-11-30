95f7194f06439ebe19762ab5e2efe1190871bbbc
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-01 00:50:00 +0530

    almost done

--- Pagination/assignment/src/configs/mails.js
+++ Pagination/assignment/src/configs/mails.js
@@ -1,5 +1,7 @@
 const nodemailer = require("nodemailer")
 
+require("dotenv").config()
+
 
 
 module.exports =  nodemailer.createTransport({
@@ -7,7 +9,7 @@ module.exports =  nodemailer.createTransport({
     port: 587,
     secure: false, // upgrade later with STARTTLS
     auth: {
-      user: "a293a9987b7947",
-      pass: "d1be38e05e4e9d",
+      user: process.env.NODE_ENV == "development" ? process.env.SMTP_DEVELOPMENT_USERNAME : process.env.SMTP_PROD_USERNAME,
+      pass: process.env.NODE_ENV == "development" ? process.env.SMTP_DEVELOPMENT_PASSWORD : process.env.SMTP_PROD_PASSWORD,
     },
   });
