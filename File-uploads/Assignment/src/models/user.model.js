03d36ecf0831853cb2c089c6469f0a4197347459
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-01 18:25:17 +0530

    multer upload code done

---
+++ File-uploads/Assignment/src/models/user.model.js
@@ -0,0 +1,9 @@
+const {Schema, model} = require("mongoose")
+
+const userSchema = new Schema({
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    profile_pic: {type: String, required: true}
+})
+
+module.exports = model("user", userSchema)
