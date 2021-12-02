bc49a718c43d7eef7da045a1531ac898570c8b04
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-02 15:57:38 +0530

    little changes

--- Validation/assign/src/models/user.model.js
+++ Validation/assign/src/models/user.model.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    id : {type: Number, required : true},
+    id : {type: Number, required : false},
     first_name : {type: String, required: true},
     last_name : {type: String, required: true},
     age: {type: String, required : true},
     email : {type : String , required :  true},
-    pincode: {type: Number, required: true},
-    gender : {type: String , required :  true},
-    ip_address : {type: String},
+    pincode: {type: Number, required: false},
+    gender : {type: String , required :  false},
+    ip_address : {type: String, required: false},
 
 }, {
     versionKey : false,
