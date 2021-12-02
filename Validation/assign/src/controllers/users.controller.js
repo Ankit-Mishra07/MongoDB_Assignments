e9bccfe73062621d816cf67d15717c3293c00468
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-02 17:17:43 +0530

    custom email validator
    custom email validator

--- Validation/assign/src/controllers/users.controller.js
+++ Validation/assign/src/controllers/users.controller.js
@@ -10,7 +10,28 @@ body("id").isLength({min: 1}).withMessage("id is required"),
 body("first_name").isLength({min: 1}).withMessage("first name is required"),
 body("last_name").isLength({min: 1}).withMessage("last name is required"),
 body("age").isLength({min:1}).withMessage("age is required"),
-body("email").isEmail().withMessage("Email.is required and it should be valid"),
+body("email").custom(async (value) => {
+    const isEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,20}$/.test(value);
+    const listOfDomain = ["gmail.com", "yahoo.com"];
+    const email = value.split("@");
+
+    if(!listOfDomain.includes(email[1])) {
+        throw new Error("We do not accept emails from this domain");
+    }
+
+    if(!isEmail) {
+        throw new Error("Please enter a proper email address");
+    }
+    const userByEmail = await User.findOne({ email : value })
+    .lean()
+    .exec()
+
+    if(userByEmail) {
+        throw new Error("Please try with a different email address")
+    }
+    return true;
+
+}),
 body("pincode").isLength({min: 6, max: 6}).withMessage("pincode should be valid"),
 body("gender").isLength({min: 3}).withMessage("gender is required must be atlease 3 character"),
 
