58fd168511ae5747ca28fe23d51266bea80c2af0
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-03 15:26:56 +0530

    learning authentication
    learning authentication

--- Authentication-Authorization/assign/src/index.js
+++ Authentication-Authorization/assign/src/index.js
@@ -7,6 +7,9 @@ app.use(express.json())
 module.exports = app
 
 const {signup, signin} = require("./controllers/auth.controller")
+const userController = require("./controllers/user.controller")
 
 app.post("/signup", signup)
 app.post("/signin", signin)
+
+app.use("/users", userController)
