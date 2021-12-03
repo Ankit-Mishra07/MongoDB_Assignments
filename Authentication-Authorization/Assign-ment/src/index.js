0ccd13f7b84edc8c4960948cad84f9cc4a46cd11
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-03 21:44:25 +0530

    post authorization
    post authorization

--- Authentication-Authorization/Assign-ment/src/index.js
+++ Authentication-Authorization/Assign-ment/src/index.js
@@ -8,6 +8,10 @@ module.exports = app
 
 const {register, login} = require("./controllers/auth.controller")
 
+const postController = require("./controllers/post.controller")
+
 app.post("/register", register)
 
 app.post("/login", login)
+
+app.use("/posts", postController)
