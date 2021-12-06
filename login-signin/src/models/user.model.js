52c9e4249c9f83f0d556a0b923adf5dc6f4159a8
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-06 18:07:49 +0530

    Learning signup authentication
    Learning signup authentication

---
+++ login-signin/src/models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const bcrypt = require("bcryptjs")
+
+const userSchema = new mongoose.Schema({
+    name : {type : String, required : false},
+    email : {type: String, required: true},
+    password : {type : String, required: true},
+    username : {type : String, required: true},
+    mobile : {type : Number, required : false}
+})
+
+userSchema.pre("save", function (next) {
+    if(!this.isModified("password")) {
+        return next()
+    }
+    const hash = bcrypt.hashSync(this.password, 10);
+    this.password = hash;
+    return next()
+})
+
+module.exports = mongoose.model("user", userSchema)
