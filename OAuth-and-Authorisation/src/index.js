a485586c91bdc0f80e8571a77f415e5007e3655e
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-07 19:45:02 +0530

    login and signup done
    login and signup done

--- OAuth-and-Authorisation/src/index.js
+++ OAuth-and-Authorisation/src/index.js
@@ -5,3 +5,8 @@ const app = express()
 app.use(express.json())
 
 module.exports = app
+
+const {register, login} = require("./controllers/auth.controller")
+
+app.post("/register", register)
+app.post("/login", login)
