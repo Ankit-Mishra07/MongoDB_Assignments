4e96d1c77fe00d79f6c97408a4435270be170a83
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-07 23:52:00 +0530

    Google oAuth is done
    Google oAuth is done

--- OAuth-and-Authorisation/src/index.js
+++ OAuth-and-Authorisation/src/index.js
@@ -4,9 +4,22 @@ const app = express()
 
 app.use(express.json())
 
-module.exports = app
+const passport = require("./configs/passport")
 
-const {register, login} = require("./controllers/auth.controller")
+
+app.use(passport.initialize());
+
+passport.serializeUser(function ({ user, token }, done) {
+    done(null, { user, token });
+});
+passport.deserializeUser(function (user, done) {
+    done(err, user);
+  });
+  
+
+  
+
+  const {register, login} = require("./controllers/auth.controller")
 const productController = require("./controllers/product.controller")
 
 app.post("/register", register)
@@ -14,3 +27,24 @@ app.post("/login", login)
 
 
 app.use("/products", productController)
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+      ));
+
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        failureRedirect: '/auth/google/failure'
+}),
+function (req, res) {
+    return res.status(201).json({user : req.user.user, token : req.user.token})
+}
+);
+
+app.get("/auth/google/failure", function (req, res) {
+    return res.send("Something went wrong")
+})
+
+
+module.exports = app
